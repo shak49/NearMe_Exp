749fca79399cb4756e953f84310c266e8112b982
Shak49 <shakfeizi@gmali.com>
2022-08-09 09:39:10 -0500

    project's files added

---
+++ NearMe_Exp/NearMe_Exp/NearMe_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMe_ExpApp.swift
+//  NearMe_Exp
+//
+//  Created by Shak Feizi on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct NearMe_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
